f765c06ba5a72cac01d4df7ae8658a96d6ea9960
AmaallA <amaaladams37@gmail.com>
2026-03-04 16:04:22 +0200

    Handle_client method completed with comments

--- TCPserver.py
+++ TCPserver.py
@@ -2,7 +2,7 @@
 import socket
 import threading
 
-serverAddres = "127.0.0.1"  # Localhost
+serverAddress = "127.0.0.1"  # Localhost
 serverPort = 1500
 
 clientSockets = []
@@ -10,14 +10,32 @@ clientSockets = []
 def handle_client(clientSocket): #handles the client connection
     while True:
         #code to handle client messages and broadcast to other clients
+        #receiving msg
+        try:
+            msg = clientSocket.recv(1042).decode    #taken from slides
+            if not msg:     #client disconnected
+                break
+
+            #broadcasting function
+            for sock in clientSockets:
+                if sock != clientSocket:        #do not send to the sender
+                    try:
+                        sock.send(msg.encode())
+                    except:         #remove dead conns
+                        clientSockets.remove(sock)
+        except:
+            break
+
         clientSocket.close()
+        if clientSocket in clientSockets:
+            clientSockets.remove(clientSocket)
 
 def start_server():
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverSocket.bind((serverAddres, serverPort))
+    serverSocket.bind((serverAddress, serverPort))
     serverSocket.listen()
 
-    print("Server is listening on {}:{}".format(serverAddres, serverPort))
+    print("Server is listening on {}:{}".format(serverAddress, serverPort))
 
     while True:
         clientSocket, clientAddress = serverSocket.accept()
