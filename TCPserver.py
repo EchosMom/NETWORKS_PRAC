9025352d9ce40c6dbfa4caeed5164c64448503e2
AmaallA <amaaladams37@gmail.com>
2026-03-04 16:50:10 +0200

    completed TCPserver.py ... Added a new separate method for broadcasting and a main method as well so that server doesnt start on import. Added more comments for understanding. imported the protocol.py file

--- TCPserver.py
+++ TCPserver.py
@@ -1,47 +1,59 @@
 """handles connections"""
 import socket
 import threading
+import protocol     #protocol module
 
 serverAddress = "127.0.0.1"  # Localhost
 serverPort = 1500
 
-clientSockets = []
+clientSockets = []          #track connected clients
 
-def handle_client(clientSocket): #handles the client connection
-    while True:
-        #code to handle client messages and broadcast to other clients
+"""handle individual client connections"""
+def handle_client(clientSocket): 
+    try:
         #receiving msg
-        try:
-            msg = clientSocket.recv(1042).decode    #taken from slides
-            if not msg:     #client disconnected
+        while True:
+            msg = clientSocket.recv(1042).decode()    #receive
+            if not msg:     #empty msg = client disconnected
                 break
 
-            #broadcasting function
-            for sock in clientSockets:
-                if sock != clientSocket:        #do not send to the sender
-                    try:
-                        sock.send(msg.encode())
-                    except:         #remove dead conns
-                        clientSockets.remove(sock)
-        except:
-            break
+            broadcast(msg, clientSocket)
 
-        clientSocket.close()
-        if clientSocket in clientSockets:
-            clientSockets.remove(clientSocket)
+    except Exception as e:
+            print("Error with client: {}".format(e))
+        
+    clientSocket.close()
+    if clientSocket in clientSockets:
+        clientSockets.remove(clientSocket)
 
+"""broadcast to all except sender"""
+def broadcast(msg, senderSocket):
+    for sock in clientSockets[:]:   #iterate over copy of list
+        if sock != senderSocket:
+            try:
+                sock.send(msg)
+            except:
+                if sock in clientSockets:
+                    clientSockets.remove(sock)      #remove failed conns
+
+"""start chat server"""
 def start_server():
-    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)        #create TCP socket
     serverSocket.bind((serverAddress, serverPort))
     serverSocket.listen()
 
     print("Server is listening on {}:{}".format(serverAddress, serverPort))
 
-    while True:
+    while True:     #server always-on
         clientSocket, clientAddress = serverSocket.accept()
         print("New connection from {}:{}".format(clientAddress[0], clientAddress[1]))
 
         clientSockets.append(clientSocket)
 
         thread =threading.Thread(target=handle_client, args=(clientSocket,))
+        thread.daemon = True        #thread closes when main program exits.
         thread.start()
+
+"""main method to start server"""
+if __name__ == "__main__":
+    start_server()
