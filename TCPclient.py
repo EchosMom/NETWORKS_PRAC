1172771aafe6518aa374cff4a212ea451de3ee1e
Kaycee <kczexplorer@gmail.com>
2026-03-05 03:59:58 +0200

    added send and receive functions

--- TCPclient.py
+++ TCPclient.py
@@ -6,12 +6,44 @@ import threading
 serverAddres = "127.0.0.1"  # Localhost
 serverPort = 1500
 
+"""Makes connection with server."""
 def connect_client():
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    clientSocket.connect((serverAddres, serverPort))
+    try:
+        clientSocket.connect((serverAddres, serverPort))
+        print("Connection successful.")
+    except:
+        print("Error: Connection failed.")
 
-    threading.Thread(target=receive_message, args=(clientSocket,)).start()
+    threading.Thread(target=receive_reply, args=(clientSocket,)).start()
+    send_request(clientSocket)
 
-def receive_message(clientSocket):
+"""Sends requests to the server."""
+def send_request(clientSocket):
     while True:
-        """Receives messages from the server and prints them to the console."""
+        request= input()
+        if request=="exit":
+            clientSocket.close()
+            break
+        try:
+            clientSocket.send(request.encode())
+        except:
+              print("Error: request not sent.")
+              break
+
+"""Receives replies from the server and prints them to the console."""
+def receive_reply(clientSocket):
+    while True:
+        try:
+            reply= clientSocket.recv(1024)
+            if not reply:
+                print("Connection ended.")
+                break
+            else:
+                print(reply.decode())
+        except:
+            print("Error: reply not received.")
+            break
+
+if __name__ == '__main__':
+    connect_client()
